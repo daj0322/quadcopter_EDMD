304ed3aabd11d8033867c21127ce68b6dd82812f
Kryptopher <Smaha12@lsu.edu>
2026-03-27 17:27:31 -0500

    Sanjay_update

--- WorkingFolder_Darren/SaveTraj.py
+++ WorkingFolder_Darren/SaveTraj.py
@@ -1,4 +1,8 @@
 from Simulation import quad_sim
 
 quad = quad_sim()
-quad.fct_save_simulation_runs(traj=1, n=5, filename="runs_traj2_n15.pkl")
+quad.fct_save_simulation_runs(traj=1, n=200, filename="runs_traj1_n200.pkl")
+
+quad.fct_save_simulation_runs(traj=2, n=100, filename="runs_traj2_n100.pkl")
+
+quad.fct_save_simulation_runs(traj=1, n=100, filename="runs_traj1_n100.pkl")
